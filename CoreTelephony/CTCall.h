d8fc759304af669699c3bb66b76c69b335766a40
Cykey <cykey@live.com>
2013-05-03 23:01:05 -0400

    CTTelephonyCenter and some more CTCall functions.

--- CoreTelephony/CTCall.h
+++ CoreTelephony/CTCall.h
@@ -40,6 +40,20 @@ void CTCallHold(CTCallRef call);
 void CTCallResume(CTCallRef call);
 void CTCallDisconnect(CTCallRef call);
 
+Boolean CTCallIsConferenced(CTCallRef call);
+Boolean CTCallIsAlerting(CTCallRef call);
+Boolean CTCallIsToVoicemail(CTCallRef call);
+Boolean CTCallIsOutgoing(CTCallRef call);
+
+/*
+ * The phone number passed in the dial functions must be normalized.
+ * E.g. +1 (555) 555-5555 would become 15555555555.
+ * One can use CPPhoneNumberCopyNormalized from AppSupport.framework to normalize phone numbers.
+ */
+
+CTCallRef CTCallDial(CFStringRef number);
+CTCallRef CTCallDialEmergency(CFStringRef number);
+
 __END_DECLS
 
 #endif /* _CTCall_H_ */
---
+++ CoreTelephony/CTTelephonyCenter.h
@@ -0,0 +1,24 @@
+/*
+ * CoreTelephony notification support.
+ */
+
+#ifndef _CTTelephonyCenter_H_
+#define _CTTelephonyCenter_H_
+
+#include "CoreHeaders.h"
+
+__BEGIN_DECLS
+
+#pragma mark - API
+
+CFNotificationCenterRef CTTelephonyCenterGetDefault(void);
+void CTTelephonyCenterAddObserver(CFNotificationCenterRef center, const void *observer, CFNotificationCallback callBack, CFStringRef name, const void *object, CFNotificationSuspensionBehavior suspensionBehavior);
+void CTTelephonyCenterRemoveObserver(CFNotificationCenterRef center, const void *observer, CFStringRef name, const void *object);
+void CTTelephonyCenterRemoveEveryObserver(CFNotificationCenterRef center, const void *observer);
+
+CFStringRef kCTCallStatusChangeNotification;
+CFStringRef kCTCallIdentificationChangeNotification;
+
+__END_DECLS
+
+#endif /* _CTTelephonyCenter_H_ */
