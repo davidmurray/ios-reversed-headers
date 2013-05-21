c36306825cf64c56173b96a233e4bbea1e06dc1a
Cykey <cykey@live.com>
2013-05-20 20:22:22 -0400

    More MobileInstallation reversing. :) Yay

--- MobileInstallation/MobileInstallation.h
+++ MobileInstallation/MobileInstallation.h
@@ -9,6 +9,8 @@
 
 __BEGIN_DECLS
 
+typedef void (*MobileInstallationCallback)(CFDictionaryRef information);
+
 /*
  * 'ReturnAttributes' is one (or an array) of the keys in the dictionary returned by MobileInstallationLookup(NULL).
  * Or 'BundleIDs' which tells installd to return only the bundle identifiers.
@@ -29,7 +31,14 @@ __BEGIN_DECLS
 CFDictionaryRef MobileInstallationLookup(CFDictionaryRef properties);
 
 /*
- * Use 0 for all 3 unknown arguments.
+ * @param bundleIdentifier: (Required) The application's bundle identfier that you wish to uninstall.
+ * @param parameters: (Optional) A dictionary of paramenters. You can safely pass NULL.
+ * @param callback: (Optional) A callback function. You can safely pass NULL.
+ * @param unknown: (Optional) Unknown. You can safely pass NULL.
+ * @return:
+ *  -1: Something went wrong. E.g your program doesn't have the necessary entitlements.
+ *   0: Everything is okay.
+ *
  * Your program needs the "com.apple.private.mobileinstall.allowedSPI" entitlement to use this function with an array containing the 'Uninstall' string.
  *
  * Example:
@@ -39,12 +48,36 @@ CFDictionaryRef MobileInstallationLookup(CFDictionaryRef properties);
  *      <string>Uninstall</string>
  *  </array>
  *
- * Return values:
- *  -1: Something went wrong. E.g your program doesn't have the necessary entitlements.
- *   0: Everything is okay.
  */
 
-int MobileInstallationUninstall(CFStringRef bundleIdentifier, int __unknown0, int __unknown1, int __unknown2);
+int MobileInstallationUninstall(CFStringRef bundleIdentifier, CFDictionaryRef parameters, MobileInstallationCallback callback, void *unknown);
+
+/*
+ * @param path: (Required) The path to the IPA file that you wish to install.
+ * @param parameters: (Optional) A dictionary of paramenters. You can safely pass NULL.
+ * @param callback: (Optional) A callback function. You can safely pass NULL.
+ * @param unknown: (Optional) Unknown. You can safely pass NULL.
+ * @return:
+ *  -1: Something went wrong. E.g your program doesn't have the necessary entitlements or the path is wrong.
+ *   0: Everything is okay.
+ *
+ *  Your program needs the "com.apple.private.mobileinstall.allowedSPI" entitlement to use this function with an array containing the 'Install' string.
+ *
+ *  <key>com.apple.private.mobileinstall.allowedSPI</key>
+ *  <array>
+ *      <string>Install</string>
+ *  </array>
+ */
+
+int MobileInstallationInstall(CFStringRef path, CFDictionaryRef parameters, MobileInstallationCallback callback, void *unknown);
+
+/*
+ * Used to rebuild the application map.
+ * See http://gitweb.saurik.com/uikittools.git/blob/HEAD:/extrainst_.mm#l114
+ * for more information.
+ */
+
+int _MobileInstallationRebuildMap(CFBooleanRef __unknown0, CFBooleanRef __unknown1, CFBooleanRef __unknown2);
 
 __END_DECLS
 
