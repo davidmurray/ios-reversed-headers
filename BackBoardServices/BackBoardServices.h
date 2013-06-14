4dc94b8e849331d6da8d656bc0686fd20015a377
Cykey <cykey@live.com>
2013-06-14 16:03:30 -0400

    Add BackBoardServices header.

---
+++ BackBoardServices/BackBoardServices.h
@@ -0,0 +1,64 @@
+/*
+ * BackBoardServices framework header.
+ */
+
+#ifndef BACKBOARDSERVICES_H_
+#define BACKBOARDSERVICES_H_
+
+#import <CoreFoundation/CoreFoundation.h>
+
+
+#if __cplusplus
+extern "C" {
+#endif
+
+#pragma mark - Definitions
+
+	#define BKSProcessAssertionFlagNone 0
+	#define BKSProcessAssertionFlagPreventSuspend (1 << 0)
+	#define BKSProcessAssertionFlagPreventThrottleDownCPU (1 << 1)
+	#define BKSProcessAssertionFlagPreventIdleSleep (1 << 2)
+	#define BKSProcessAssertionFlagWantsForegroundResourcePriority (1 << 3)
+
+	typedef enum {
+		BKSProcessAssertionReasonNone,
+		BKSProcessAssertionReasonAudio,
+		BKSProcessAssertionReasonLocation,
+		BKSProcessAssertionReasonExternalAccessory,
+		BKSProcessAssertionReasonFinishTask,
+		BKSProcessAssertionReasonBluetooth,
+		BKSProcessAssertionReasonNetworkAuthentication,
+		BKSProcessAssertionReasonBackgroundUI,
+		BKSProcessAssertionReasonInterAppAudioStreaming,
+		BKSProcessAssertionReasonViewServices
+	} BKSProcessAssertionReason;
+
+	typedef struct BKSDisplayBrightnessTransaction *BKSDisplayBrightnessTransactionRef;
+
+#pragma mark - API
+	/* Level should be fron 0.0 to 1.0. */
+	void BKSDisplayBrightnessSet(float level, int _unknown /* use 1 */);
+
+	/* Returns a value from 0.0 to 1.0. */
+	float BKSDisplayBrightnessGetCurrent();
+
+	/* Follows the 'Create' rule. */
+	BKSDisplayBrightnessTransactionRef BKSDisplayBrightnessTransactionCreate(CFAllocatorRef allocator);
+
+	void BKSDisplayBrightnessSetAutoBrightnessEnabled(Boolean enabled);
+
+	void BKSHIDServicesSetBacklightFactorWithFadeDuration(float factor, int duration);
+
+	/* Check out http://iphonedevwiki.net/index.php/BackBoardServices.framework
+	 * for more information on BKSTerminate functions.
+	 */
+
+	void BKSTerminateApplicationForReasonAndReportWithDescription(CFStringRef applicationIdentifier, int __unknown0, int __unknown1, CFStringRef description);
+
+	void BKSTerminateApplicationGroupForReasonAndReportWithDescription(int __unknown0, int __unknown1, int __unknown2, CFStringRef description);
+
+#if __cplusplus
+}
+#endif
+
+#endif /* BACKBOARDSERVICES_H_ */
