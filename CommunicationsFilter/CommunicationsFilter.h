5210ca6a8df2e228f3cf5801c4d7ece5642bc6b3
Pb <scandium@me.com>
2014-08-01 00:09:54 +0300

    CommunicationsFilter.framework

---
+++ CommunicationsFilter/CommunicationsFilter.h
@@ -0,0 +1,22 @@
+/*
+ * CommunicationsFilter framework header
+ *
+ * Copyright (c) 2013-2014 Plumbum
+ * All rights reserved.
+ */
+
+// Check http://github.com/comscandiumplumbumd/CMFExample for usage example
+
+// Needs com.apple.private.communicationsfilter entitlement
+
+#ifndef CommunicationsFilter_h
+#define CommunicationsFilter_h
+
+#ifdef __OBJC__
+    #include "CommunicationFilterItem.h"
+    #include "CommunicationFilterItemCache.h"
+    #include "CommunicationsFilterBlockList.h"
+    #include "CommunicationsFilterBlockListCache.h"
+#endif
+
+#endif
